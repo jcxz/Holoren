d3c0735dd9b0224ecc8b31525ad1439ee1b73a87
matus <matus@A05-109a.(none)>
2013-05-13 19:13:15 +0200

    Added performance test results from Ubuntu and fixed CMakeLists.txt to include the timing support even in release mode

--- perf_tests/run_tests.sh
+++ perf_tests/run_tests.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/bash
 
-EXE_NAME=IBP_holoren
+EXE_NAME="../build/release/holoren"
 OF_W=1024
 OF_H=1024
-INPUT="../../../input/img/FIT-logo_160x160.jpg"
-KERNEL="../../../src/OpenCL/holoren_obj_wave.cl"
+INPUT="../input/img/FIT-logo_160x160.png"
+KERNEL="../src/OpenCL/holoren_obj_wave.cl"
 
 
 # run the first set of tests (MUlti pass algorithm on CPU)
-PERF_LOG="../../../perf_tests/Ubuntu/perf_MultiPassCPU.txt"
-OUTPUT="../../../perf_tests/Ubuntu/MultiPassCPU.df"
+PERF_LOG="./Ubuntu/perf_MultiPassCPU.txt"
+OUTPUT="./Ubuntu/MultiPassCPU.df"
 ALG=MultiPassCPU
 
 for i in `seq 5`; do
@@ -18,17 +18,18 @@ done
 
 
 # run the second set of tests (multipass algorithm on GPU)
-PERF_LOG="../../../perf_tests/Ubuntu/perf_MultiPass.txt"
-OUTPUT="../../../perf_tests/Ubuntu/MultiPass.df"
+PERF_LOG="./Ubuntu/perf_MultiPass.txt"
+OUTPUT="./Ubuntu/MultiPass.df"
 ALG=MultiPass
 
 for i in `seq 5`; do
   "${EXE_NAME}" -z 0.5 -s 0.00002 -w ${OF_W} -h ${OF_H} -i "${INPUT}" -a ${ALG} -r opencl -o "${OUTPUT}" -f "${KERNEL}" -p "${PERF_LOG}"
 done
 
+
 # run the third set of tests  (multipass algorithm on GPU utilising native instructions)
-PERF_LOG="../../../perf_tests/Ubuntu/perf_MultiPassNative.txt"
-OUTPUT="../../../perf_tests/Ubuntu/MultiPassNative.df"
+PERF_LOG="./Ubuntu/perf_MultiPassNative.txt"
+OUTPUT="./Ubuntu/MultiPassNative.df"
 ALG=MultiPassNative
 
 for i in `seq 5`; do
